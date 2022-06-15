d1fea776f0933df7ae819a08d9fa3e948cd856ee
yn1323 <yn1323@gmail.com>
2022-06-16 01:50:04 +0900

    fix: storybook interaction

--- web/config/storybook.config.js
+++ web/config/storybook.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  addons: ['@chakra-ui/storybook-addon'],
+  addons: ['@chakra-ui/storybook-addon', '@storybook/addon-interactions'],
   // NOTE: なぜかこれで早くなる
   // https://github.com/storybookjs/storybook/issues/10784
   typescript: {
--- web/src/pages/TopPage/TopPage.tsx
+++ web/src/pages/TopPage/TopPage.tsx
@@ -1,15 +1,22 @@
+import { FC } from 'react'
+
 import { Button } from '@chakra-ui/react'
 
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 
-const TopPage = () => {
-  console.log(process.env.MOGMOG)
+type PropTypes = {
+  show: boolean
+  hoge?: '11' | '22'
+}
+
+const TopPage: FC<PropTypes> = ({ show, hoge }) => {
   return (
     <>
       <MetaTags title="Top" description="Top page" />
       <h1>TopPage</h1>
-      <Button>aaaa</Button>
+      {show && <Button data-testid="close">aaaa</Button>}
+      {!show && <Button data-testid="close">not Show</Button>}
       <p>
         Find me in <code>./web/src/pages/TopPage/TopPage.tsx</code>
       </p>
