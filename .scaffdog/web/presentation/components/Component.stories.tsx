36851445e825b05da9aa932f045014ba4af02529
yn1323 <yn1323@gmail.com>
2022-09-01 23:45:48 +0900

    fix: scaffdog

--- .scaffdog/web/presentation/components/Component.stories.tsx
+++ .scaffdog/web/presentation/components/Component.stories.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import { ComponentStoryObj, ComponentMeta } from '@storybook/react'
 // import { userEvent, within, screen } from '@storybook/testing-library'
 
